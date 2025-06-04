df9472dff405975719a6fb7319098005048950a1
canoekuro <136628011+canoekuro@users.noreply.github.com>
2025-06-04 17:45:48 +0900

    Update test.py

--- files/test.py
+++ files/test.py
@@ -1,5 +1,7 @@
 import logging
 from files.backend_codes import build_workflow
+from langchain_core.messages import HumanMessage
+
 
 # 基本的なロギングを設定
 logging.basicConfig(level=logging.INFO)
@@ -9,16 +11,13 @@ logging.basicConfig(level=logging.INFO)
 workflow = build_workflow()
 
 # 例：ユーザー入力から初期状態を作って実行
-state = {
-    "messages": [HumanMessage(content="データベース内のテーブルの一覧を取得したい")],
-    "task_description": [],
-    "metadata_answer": [],
-    "df_history": [],
-    "sql_history": [],
-    "interpretation_history": [],
-    "chart_history": [],
-    "analize_step": []
-}
 
-result = workflow.invoke(state, config={"thread_id": "my_test_session_001"})
+state = {
+    "messages": [
+        HumanMessage(content="利用可能なデータの内容が知りたい。")
+    ],
+    "next": "supervisor",
+    "df_history": []
+}
+result = workflow.invoke(state, config={"thread_id": "my_test_session_001","return_intermediate_steps": True})
 print(result)
