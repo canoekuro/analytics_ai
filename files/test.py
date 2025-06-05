f73ccac85aeaec510c4e2260934bc9278c5df83f
canoekuro <canoekuro@gmail.com>
2025-06-05 11:15:14 +0900

    ちょっとだけ修正

--- files/test.py
+++ files/test.py
@@ -1,17 +1,13 @@
 import logging
-from files.backend_codes import build_workflow
+from backend_codes import build_workflow
 from langchain_core.messages import HumanMessage
 
 
 # 基本的なロギングを設定
 logging.basicConfig(level=logging.INFO)
 
-from langchain_core.messages import HumanMessage
-logging.basicConfig(level=logging.INFO)
 workflow = build_workflow()
-
 # 例：ユーザー入力から初期状態を作って実行
-
 state = {
     "messages": [
         HumanMessage(content="利用可能なデータの内容が知りたい。")
